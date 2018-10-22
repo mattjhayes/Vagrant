966125bebe795cd1eb4a472fe112df5b0d619d43
Matt Hayes <matthew_john_hayes@hotmail.com>
2018-10-22 13:45:29 +1300

    Initial commit of IP Fragmentation Lab in Vagrant

---
+++ Network_Test_Labs/Fragmentation_IPv4/bootstrap_pc1.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#*** Provisioning Script that runs in the guest box
+
+#*** Provision generic client guest configuration:
+
+#*** Set up aliases from file in synced_folder:
+cp /vagrant/aliases_pc1 ~/.bash_aliases
+
+#*** Read in aliases for current session:
+source ~/.bashrc
+
+# Add static routes:
+sudo ip route add 10.2.0.0/24 via 10.1.0.1
+sudo ip route add 10.3.0.0/24 via 10.1.0.1
---
+++ Network_Test_Labs/Fragmentation_IPv4/bootstrap_rt2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#*** Provisioning Script that runs in the guest box
+
+#*** Provision generic client guest configuration:
+
+#*** Set up aliases from file in synced_folder:
+cp /vagrant/aliases_rt1 ~/.bash_aliases
+
+#*** Read in aliases for current session:
+source ~/.bashrc
+
+# Enable IP forwarding:
+echo "1" | sudo tee /proc/sys/net/ipv4/ip_forward
+
+# Add static route:
+sudo ip route add 10.1.0.0/24 via 10.2.0.1
+
+# Reduce MTU on connection between routers to cause IP fragmentation
+sudo ifconfig eth2 mtu 1200
